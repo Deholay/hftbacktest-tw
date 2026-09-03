4a406b870b5ad096acd4e8e1f72ee1a7c4e7306f
Deholay <zoufuc.gss@icloud.com>
2026-09-03 17:32:42 +0800

    ci: fix py3.10 test error

--- tests/test_public_api.py
+++ tests/test_public_api.py
@@ -2,8 +2,12 @@ from __future__ import annotations
 
 import dataclasses
 import math
-import tomllib
 from pathlib import Path
+
+try:
+    import tomllib
+except ModuleNotFoundError:  # Python 3.10
+    import tomli as tomllib
 
 import pytest
 
